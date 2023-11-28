91ba77f6a1d6235b66ab0180a13a7d8464e9feb0
churikov-sv <churikov.sv@outlook.com>
2023-11-29 01:10:54 +0300

    Task code program.cs

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿string[] firstArray = new string[5] {"1234", "1567", "-2", "computer science", "123"};
+string[] secondArray = new string[firstArray.Length];
+
+
+string[] ArrayStringsLenMin(string[] firstArray, string[] secondArray)
+{
+    int count = 0;
+    
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+    if(firstArray[i].Length <= 3)
+        {
+        secondArray[count] = firstArray[i];
+        count++;
+        }
+    }
+
+    return secondArray;
+}
+
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]} ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+string[] arrayStringsLenMin = ArrayStringsLenMin(firstArray, secondArray);
+
+Console.Write($"[");
+PrintArray(arrayStringsLenMin);
+Console.Write($"]");
